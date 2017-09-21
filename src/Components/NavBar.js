cc4c7f5346fc235530181214f0f11567d751977e
Imani Oliver <isaac@ihardy.co>
2017-09-21 17:28:46 -0400

    navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,27 +7,25 @@ export default class NavBar extends Component {
     render(){
         return(
 
+                <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                    <ul class="nav">
+                      <li class="nav-item">
+                        <NavLink class="nav-link active" to="#">BēânLoft</NavLink>
+                      </li>
+                      <li class="nav-item">
+                        <NavLink class="nav-link" to="#">Products</NavLink>
+                      </li>
+                      <li class="nav-item">
+                        <NavLink class="nav-link" to="#">Features</NavLink>
+                      </li>
+                      <li class="nav-item">
+                        <NavLink class="nav-link" to="#section1">Pricing</NavLink>
+                      </li>
+                    </ul>
+                </nav>
 
-            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <NavLink className="navbar-brand" to="1">Tiny Bēân</NavLink>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item active">
-                    <NavLink className="nav-link" to="">Home <span className="sr-only">(current)</span></NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="#">Features</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <a href='#section1'> Go to prices </a>
-                  </li>
 
-                </ul>
-              </div>
-            </nav>
+
 
 
 
